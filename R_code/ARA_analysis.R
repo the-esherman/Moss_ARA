b06f807322347973ef9b6cb9c52d4ee324d9b0b0
the-esherman <emiltheemperor@gmail.com>
2023-11-29 12:12:36 +0100

    Begun main calculations of ARA. Time, Acetylene loss, ethylene production

--- R_code/ARA_analysis.R
+++ R_code/ARA_analysis.R
@@ -3,6 +3,7 @@
 # 
 #=======  ♣   Libraries     ♣ =======
 library(tidyverse)
+library(lubridate)
 library(car)
 library(nlme)
 #
@@ -11,12 +12,16 @@ library(nlme)
 #=======  ♠   Load data     ♠ =======
 # Import ID's
 ID_info <- read_xlsx("Data_raw/Data_ID.xlsx")
-
 #
+# Import field and vial datasets
 field_ARA <- read_csv("Data_clean/field_ARA.csv", col_names = TRUE)
 vial_ARA <- read_csv("Data_clean/vial_ARA.csv", col_names = TRUE)
-
-
+#
+# Chamber size
+Ch_H <- 7 # Chamber height (cm) above moss surface. This needs to be changed to match each plot estimated height
+Ch_r <- 5 # Chamber radius (cm)
+Ch_vol_L <- (Ch_r^2*pi*Ch_H)/1000 # Chamber vol in L
+Ch_area_m2 <- (Ch_r^2*pi)/10000 # Chamber area in m2
 #
 #
 #
@@ -26,11 +31,24 @@ vial_ARA <- read_csv("Data_clean/vial_ARA.csv", col_names = TRUE)
 #
 #
 #=======  ♦   Main data     ♦ =======
-
+#
+#
+#------- • Field data -------
+#
+# Remove chamber test
+field_ARA.2 <- field_ARA %>%
+  filter(Species != "B")
+#
+# Pivot wider and combine:
+# Timestamp
+field_ARA.Time <- field_ARA.2 %>% 
+  select(!c(Time_nr, Date, Chamber_no, Chain_type, Temp_approx_C, Ethyl_conc_ppm, Acet_conc_prC)) %>%
+  complete(Block, Species, Time, Round) %>%
+  pivot_wider(names_from = Time, values_from = Timestamp)
 #
 # Ethylene
-field_ARA.Ethyl <- field_ARA %>% 
-  select(!c(Sample_ID, Acet_conc_prC)) %>%
+field_ARA.Ethyl <- field_ARA.2 %>% 
+  select(!c(Time_nr, Date, Timestamp, Chamber_no, Chain_type, Temp_approx_C, Acet_conc_prC)) %>%
   complete(Block, Species, Time, Round) %>%
   pivot_wider(names_from = Time, values_from = Ethyl_conc_ppm) %>%
   rename("Ethyl_conc_ppm_T0" = T0,
@@ -42,23 +60,49 @@ field_ARA.Ethyl <- field_ARA %>%
          "Ethyl_conc_ppm_T4" = T4)
 #
 # Acetylene
-field_ARA.Acet <- field_ARA %>% 
-  select(!c(Sample_ID, Ethyl_conc_ppm)) %>%
+field_ARA.Acet <- field_ARA.2 %>% 
+  select(!c(Time_nr, Date, Timestamp, Chamber_no, Chain_type, Temp_approx_C, Ethyl_conc_ppm)) %>%
   complete(Block, Species, Time, Round) %>%
-  pivot_wider(names_from = Time, values_from = Acet_conc_prC) %>%
-  rename("Acet_conc_prC_T0" = T0,
-         "Acet_conc_prC_T0x" = T0x,
-         "Acet_conc_prC_T1" = T1,
-         "Acet_conc_prC_T1x" = T1x,
-         "Acet_conc_prC_T2" = T2,
-         "Acet_conc_prC_T3" = T3,
-         "Acet_conc_prC_T4" = T4)
+  mutate(Acet_conc_ppm = Acet_conc_prC*10000) %>%
+  select(!Acet_conc_prC) %>%
+  pivot_wider(names_from = Time, values_from = Acet_conc_ppm) %>%
+  rename("Acet_conc_ppm_T0" = T0,
+         "Acet_conc_ppm_T0x" = T0x,
+         "Acet_conc_ppm_T1" = T1,
+         "Acet_conc_ppm_T1x" = T1x,
+         "Acet_conc_ppm_T2" = T2,
+         "Acet_conc_ppm_T3" = T3,
+         "Acet_conc_ppm_T4" = T4)
 #
-field_ARA.2 <- left_join(field_ARA.Ethyl, field_ARA.Acet, by = join_by(Block, Species, Round))
+# Join 
+field_ARA_wide <- left_join(field_ARA.Time, field_ARA.Ethyl, by = join_by(Block, Species, Round)) %>%
+  left_join(field_ARA.Acet, by = join_by(Block, Species, Round))
 #
-field_ARA.3 <- left_join(field_ARA, ID_info, by = join_by())
+# Do the math
+x <- field_ARA_wide %>%
+  # Calculate the time difference from T_n-1 to T_n
+  mutate(Time1 = hour(seconds_to_period(T1 - T0))*60 + minute(seconds_to_period(T1 - T0)),
+         Time2 = hour(seconds_to_period(T2 - T1))*60 + minute(seconds_to_period(T2 - T1)),
+         Time3 = hour(seconds_to_period(T3 - T2))*60 + minute(seconds_to_period(T3 - T2)),
+         Time4 = hour(seconds_to_period(T4 - T3))*60 + minute(seconds_to_period(T4 - T3))) %>%
+  # Calculate the Loss of acetylene over time
+  mutate(Acet_loss1 = (Acet_conc_ppm_T0 - Acet_conc_ppm_T1)/Acet_conc_ppm_T0*100,
+         Acet_loss2 = (Acet_conc_ppm_T0 - Acet_conc_ppm_T2)/Acet_conc_ppm_T0*100,
+         Acet_loss3 = (Acet_conc_ppm_T0 - Acet_conc_ppm_T3)/Acet_conc_ppm_T0*100,
+         Acet_loss4 = (Acet_conc_ppm_T0 - Acet_conc_ppm_T4)/Acet_conc_ppm_T0*100)
+#
+# Some of the Acetylene values are well below 0, and should probably be corrected.
+# Three samples have negative starting values (T0), but then subsequently high positive values at T1
+#
+xx <- x %>%
+  mutate(Et_prod_ppm_pr_h_1 = (Ethyl_conc_ppm_T0 - Ethyl_conc_ppm_T1)/(Time1/60))
 
+#
+#
+#------- • Vial data -------
 
+# ??
+mutate(across(Timestamp, ~hm(.x)))# %>%
 #
 #
 #
