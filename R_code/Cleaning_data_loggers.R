498228303ff2d31d2d2aa0dd37fb671864b940d5
the-esherman <emiltheemperor@gmail.com>
2023-12-14 11:56:30 +0100

    Explanation of TinyTags and daylight saving time mess. Corrected for the Wetland TinyTag.

--- R_code/Cleaning_data_loggers.R
+++ R_code/Cleaning_data_loggers.R
@@ -8,11 +8,25 @@
 library(tidyverse)
 library(readxl)
 library(lubridate)
+library(hms)
 #
 #
 #
 #=======  ♠   TinyTag     ♠ =======
 # Load TinyTag temperature measurements
+#
+# Some information on TinyTag dates and daylight saving time (DST)
+# See also https://www.geminidataloggers.com/support/tinytag-explorer-software/dates-and-times
+#
+#
+# TinyTag logs as set by the computer when started, so it is important that time is correct on computer before starting measurements, including timezone!
+# Furthermore, when data is downloaded on .ttd files it is kept as when launched. But when opening this .ttd file it is converted based on if it is currently DST or not.
+# If the logger is recording from winter, but read in summer, it will display all times in DST, so 18:00 becomes 19:00, but if read in winter it will be 18:00. And vice versa for summer start, measured in winter being changed back one hour
+# E.g.
+# A measurement is started January 1st and continues to June 1st when data is downloaded.
+# When the measurement (.ttd file) is read on June 1st they are converted to DST, so all values have 1 hour added. This is correct for summer, but wrong for winter. If the .ttd file is then read again in December, all values are now no longer DST: Correct for winter measurements, but wrong for summer.
+# [I hate DST, why do we need it?!?]
+#
 #
 # Heath habitat
 TinyTag_heath_202101 <- read_csv("Data_raw/Loggers/AirT Heath 202009-202101.csv", skip = 5, col_names = c("Record", "Date_time", "Max_Temp", "AirT", "Min_Temp"))
@@ -31,10 +45,13 @@ TinyTag_heath <- bind_rows(list(TinyTag_heath_202101, TinyTag_heath_202106, Tiny
 # Change accordingly, if only using average temperature and not max and min 
 TinyTag_heath <- TinyTag_heath %>% # Split temperature from the unit and Date and time. Set temperature as numeric
   separate(AirT, sep = " ", into = c("AirT", "Unit")) %>%
-  separate(Date_time, sep = " ", into = c("Date", "Time")) %>%
+  separate(Date_time, sep = " ", into = c("Day", "Time")) %>%
   separate(Time, sep = ":", into = c("hour", "min", "sec")) %>%
   unite(hour, min, col = "Time", sep = ":") %>%
-  unite(Date, Time, col = "Day_ID", sep = " ") %>%
+  mutate(Date = Day,
+         Timestamp = Time) %>%
+  relocate(Timestamp, .after = Date) %>%
+  unite(Day, Time, col = "Day_ID", sep = " ") %>%
   select(!"sec") %>%
   mutate(across(c(AirT), as.numeric))
 #
@@ -45,11 +62,21 @@ TinyTag_heath %>%
 #
 # Missing values from August to December is from logger no longer connecting. Unsure if data available
 #
+# Check for duplicates
+x <- TinyTag_heath %>%
+  distinct()
+#
+xx <- TinyTag_heath %>%
+  filter(n() > 1, .by = c(Day_ID, AirT))
+# No duplicates
+#
 #
 # Wetland habitat
 TinyTag_wetland_202101 <- read_csv("Data_raw/Loggers/AirT Wetland 202007-202101.csv", skip = 5, col_names = c("Record", "Date_time", "Max_Temp", "AirT", "Min_Temp"))
+# All the same values from 202101 are found in the next download in 202102:
 TinyTag_wetland_202102 <- read_csv("Data_raw/Loggers/AirT Wetland 20210219.csv", skip = 5, col_names = c("Record", "Date_time", "Max_Temp", "AirT", "Min_Temp"))
 TinyTag_wetland_202106 <- read_csv("Data_raw/Loggers/AirT Wetland 202106.csv", skip = 5, col_names = c("Record", "Date_time", "Max_Temp", "AirT", "Min_Temp"))
+# All values from this are found in Wetland 20210824. Discrepancies in time follows since this file was created on wintertime, while the other was created on DST!
 #
 # Duplicate?
 #TinyTag_wetland_202106 <- read_csv("Data_raw/Loggers/AirT Wetland all 20210603.csv", skip = 5, col_names = c("Record", "Date_time", "Max_Temp", "AirT", "Min_Temp"))
@@ -58,23 +85,63 @@ TinyTag_wetland_202108 <- read_csv("Data_raw/Loggers/AirT Wetland 20210824.csv",
 TinyTag_wetland_202112 <- read_csv("Data_raw/Loggers/AirtT Wetland 20211201.csv", skip = 5, col_names = c("Record", "Date_time", "Max_Temp", "AirT", "Min_Temp"))
 TinyTag_wetland_202207 <- read_csv("Data_raw/Loggers/AirT Wetland 20220721 (until 0406).csv", skip = 5, col_names = c("Record", "Date_time", "Max_Temp", "AirT", "Min_Temp"))
 #
-TinyTag_wetland <- bind_rows(list(TinyTag_wetland_202101, TinyTag_wetland_202102, TinyTag_wetland_202106, TinyTag_wetland_202108, TinyTag_wetland_202112, TinyTag_wetland_202207))
-# Change accordingly, if only using average temperature and not max and min 
-TinyTag_wetland <- TinyTag_wetland %>% # Split temperature from the unit and Date and time. Set temperature as numeric
-  separate(AirT, sep = " ", into = c("AirT", "Unit")) %>%
-  separate(Date_time, sep = " ", into = c("Date", "Time")) %>%
-  separate(Time, sep = ":", into = c("hour", "min", "sec")) %>%
-  unite(hour, min, col = "Time", sep = ":") %>%
-  unite(Date, Time, col = "Day_ID", sep = " ") %>%
-  select(!"sec") %>%
-  mutate(across(c(AirT), as.numeric))
+# Combine
+TinyTag_wetland <- bind_rows(list(TinyTag_wetland_202101, TinyTag_wetland_202102, TinyTag_wetland_202106, TinyTag_wetland_202108, TinyTag_wetland_202112, TinyTag_wetland_202207), .id = "id_file")
 #
-# Graph temperature values
-TinyTag_wetland %>%  
-  mutate(across(Day_ID, ~ymd_hm(.x))) %>% 
-  ggplot() + geom_line(aes(x = Day_ID, y = AirT))
-
-
+# Separate temperature from unit, remove duplicate measurements. Correct for DST
+TinyTag_wetland.1 <- TinyTag_wetland %>%
+  separate(AirT, sep = " ", into = c("AirT", "Unit")) %>%           
+  separate(Max_Temp, sep = " ", into = c("Max_Temp_C", "UnitMax")) %>%
+  separate(Min_Temp, sep = " ", into = c("Min_Temp_C", "UnitMin")) %>%
+  mutate(across(AirT, as.numeric)) %>%
+  mutate(id_file = case_when(id_file == "1" ~ "w202101",
+                             id_file == "2" ~ "w202102",
+                             id_file == "3" ~ "w202106",
+                             id_file == "4" ~ "w202108",
+                             id_file == "5" ~ "w202112",
+                             id_file == "6" ~ "w202207")) %>%
+  mutate(across(Date_time, ~ymd_hms(.x))) %>%
+  # Some changes need to be made as to correct for when measurements were actually taken with respect to DST
+  # w202101 was started on DST, but saved in Winter - Not used
+  # w202102 was started on DST, but saved in Winter
+  # w202106 was started in Winter, and saved in Winter !! - Not used
+  # w202108 was started in Winter, but saved on DST
+  # w202112 was started on DST, but saved in Winter - Not used
+  # w202207 was started on DST, and saved on DST !!
+  filter(id_file == "w202102" | id_file == "w202108" | id_file == "w202207") %>%
+  #
+  # Convert to UTC+1
+  mutate(Tid = case_when(id_file == "w202102" & Date_time < ymd_hm("20201025T03:00") ~ Date_time+hours(1),
+                         #id_file == "w202106" & Date_time > ymd_hm("20210328T03:00") ~ Date_time+hours(1),
+                         id_file == "w202108" & Date_time < ymd_hm("20210328T03:00") ~ Date_time-hours(1),
+                         #id_file == "w202112" & Date_time < ymd_hm("20211031T03:00") ~ Date_time+hours(1),
+                         id_file == "w202207" & Date_time > ymd_hm("20211031T03:00") & Date_time < ymd_hm("20220327T03:00") ~ Date_time-hours(1),
+                         TRUE ~ Date_time))
+#
+# Plot 
+TinyTag_wetland.1 %>%
+  mutate(across(c(Date_time, Tid), ~ymd_hms(.x))) %>%
+  ggplot(aes(x = Tid, y = AirT, color = id_file)) + geom_line()
+#
+# Separate day and time
+TinyTag_wetland.2 <- TinyTag_wetland.1 %>%
+  mutate(Date = date(Tid),
+         Time = as_hms(Tid)) %>%
+  relocate(c(Date, Time, Tid), .after = Record) %>%
+  select(!c("Date_time", "Unit", "UnitMax", "UnitMin")) %>%
+  rename("Date_time" = Tid,
+         "AirT_C" = AirT)
+#
+# Check for duplicates
+x <- TinyTag_wetland.2 %>%
+  distinct()
+#
+xx <- TinyTag_wetland.2 %>%
+  filter(n() > 1, .by = c(Date_time, AirT_C))
+# No duplicates
+#
+# Save Wetland Air temperature
+write_csv(TinyTag_wetland.2, "Data_clean/AirT_wetland.csv", na = "NA")
 #
 #
 #
